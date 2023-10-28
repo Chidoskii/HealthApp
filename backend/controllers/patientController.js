bd950822f9a9661cca6f2aeb1dd3a6b7b7548d96
Chidoskii <51214655+Chidoskii@users.noreply.github.com>
2023-10-28 08:51:24 -0700

    Routes and CRUD functions

---
+++ backend/controllers/patientController.js
@@ -0,0 +1,124 @@
+const { default: mongoose } = require('mongoose');
+const Patient = require('../models/patientModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: '1d' });
+};
+
+// login patient
+const loginPatient = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const patient = await Patient.login(email, password);
+
+    // create token
+    const token = createToken(patient._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// signup patient
+const signupPatient = async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+  try {
+    const patient = await Patient.signup(fname, lname, email, password);
+
+    // create token
+    const token = createToken(patient._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// get all patients
+const getAllPatients = async (req, res) => {
+  try {
+    const patients = await Patient.find({}).sort({ createdAt: -1 });
+    res.status(200).json(patients);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// get a single patient
+const getOnePatient = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such patient' });
+  }
+  try {
+    const { id } = req.params;
+    const patient = await Patient.findById(id);
+    res.status(200).json(patient);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// create a new patient
+const createPatient = async (req, res) => {
+  try {
+    const patient = await Patient.create(req.body);
+    res.status(200).json(patient);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// delete a patient
+const deletePatient = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such patient' });
+  }
+  try {
+    const patient = await Patient.findByIdAndDelete(id);
+    if (!patient) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any patients with ID ${id}` });
+    }
+    res.status(200).json({ message: `deleted the following ${patient}` });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// update a patient
+const updatePatient = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such patient' });
+  }
+  try {
+    const patient = await Patient.findByIdAndUpdate(id, req.body);
+    if (!patient) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any patients with ID ${id}` });
+    }
+    const updatedPatient = await Patient.findById(id);
+    res.status(200).json(updatedPatient);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createPatient,
+  getAllPatients,
+  getOnePatient,
+  deletePatient,
+  updatePatient,
+  signupPatient,
+  loginPatient,
+};
---
+++ backend/models/doctorModel.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+
+const patientSchema = mongoose.Schema(
+  {
+    patientID: {
+      type: Number,
+    },
+    fname: {
+      type: String,
+      required: [true, 'Enter a first name'],
+    },
+    lname: {
+      type: String,
+      required: [true, 'Enter a last name'],
+    },
+    mname: {
+      type: String,
+    },
+    sffx: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, 'Enter email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Enter a password'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// static signup method
+patientSchema.statics.signup = async function (fname, lname, email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error('Fill in all fields');
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Enter a valid email');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password must contain: A-Z a-z 0-9 and a special character');
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error('Try using a different email');
+  }
+
+  const crystals = await bcrypt.genSalt(10);
+  const hashish = await bcrypt.hash(password, crystals);
+
+  const patient = await this.create({ fname, lname, email, password: hashish });
+
+  return patient;
+};
+
+// static login method
+patientSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error('Fill in all fields');
+  }
+
+  const patient = await this.findOne({ email });
+
+  if (!patient) {
+    throw Error('Invalid email/password');
+  }
+
+  const match = await bcrypt.compare(password, patient.password);
+
+  if (!match) {
+    throw Error('Invalid email/password');
+  }
+
+  return patient;
+};
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+module.exports = Patient;
