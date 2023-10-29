82155155459093e96ce6f48e8f5d0a21086b306a
Chidoskii <51214655+Chidoskii@users.noreply.github.com>
2023-10-29 10:38:04 -0700

    add routes

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,124 @@
+const { default: mongoose } = require('mongoose');
+const Admin = require('../models/adminModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: '1d' });
+};
+
+// login admin
+const loginAdmin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const admin = await Admin.login(email, password);
+
+    // create token
+    const token = createToken(admin._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// signup admin
+const signupAdmin = async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+  try {
+    const admin = await Admin.signup(fname, lname, email, password);
+
+    // create token
+    const token = createToken(admin._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// get all admins
+const getAllAdmins = async (req, res) => {
+  try {
+    const admins = await Admin.find({}).sort({ createdAt: -1 });
+    res.status(200).json(admins);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// get a single admin
+const getOneAdmin = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such admin' });
+  }
+  try {
+    const { id } = req.params;
+    const admin = await Admin.findById(id);
+    res.status(200).json(admin);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// create a new admin
+const createAdmin = async (req, res) => {
+  try {
+    const admin = await Admin.create(req.body);
+    res.status(200).json(admin);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// delete a admin
+const deleteAdmin = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such admin' });
+  }
+  try {
+    const admin = await Admin.findByIdAndDelete(id);
+    if (!admin) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any admins with ID ${id}` });
+    }
+    res.status(200).json({ message: `deleted the following ${admin}` });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// update a admin
+const updateAdmin = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such admin' });
+  }
+  try {
+    const admin = await Admin.findByIdAndUpdate(id, req.body);
+    if (!admin) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any admins with ID ${id}` });
+    }
+    const updatedAdmin = await Admin.findById(id);
+    res.status(200).json(updatedAdmin);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createAdmin,
+  getAllAdmins,
+  getOneAdmin,
+  deleteAdmin,
+  updateAdmin,
+  signupAdmin,
+  loginAdmin,
+};
---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,124 @@
+const { default: mongoose } = require('mongoose');
+const Doctor = require('../models/doctorModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: '1d' });
+};
+
+// login doctor
+const loginDoctor = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const doctor = await Doctor.login(email, password);
+
+    // create token
+    const token = createToken(doctor._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// signup doctor
+const signupDoctor = async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+  try {
+    const doctor = await Doctor.signup(fname, lname, email, password);
+
+    // create token
+    const token = createToken(doctor._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// get all doctors
+const getAllDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctor.find({}).sort({ createdAt: -1 });
+    res.status(200).json(doctors);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// get a single doctor
+const getOneDoctor = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such doctor' });
+  }
+  try {
+    const { id } = req.params;
+    const doctor = await Doctor.findById(id);
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// create a new doctor
+const createDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctor.create(req.body);
+    res.status(200).json(doctor);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// delete a doctor
+const deleteDoctor = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such doctor' });
+  }
+  try {
+    const doctor = await Doctor.findByIdAndDelete(id);
+    if (!doctor) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any doctors with ID ${id}` });
+    }
+    res.status(200).json({ message: `deleted the following ${doctor}` });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// update a doctor
+const updateDoctor = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such doctor' });
+  }
+  try {
+    const doctor = await Doctor.findByIdAndUpdate(id, req.body);
+    if (!doctor) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any doctors with ID ${id}` });
+    }
+    const updatedDoctor = await Doctor.findById(id);
+    res.status(200).json(updatedDoctor);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createDoctor,
+  getAllDoctors,
+  getOneDoctor,
+  deleteDoctor,
+  updateDoctor,
+  signupDoctor,
+  loginDoctor,
+};
