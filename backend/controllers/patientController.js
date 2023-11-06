5c5115e749c14ffd5d29dcb135ce4de2824d1997
Chidoskii <51214655+Chidoskii@users.noreply.github.com>
2023-11-06 10:04:29 -0800

    change in getPatient function

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -64,8 +64,6 @@ const getOnePatient = async (req, res) => {
 // get a single patient by email
 const getPatient = async (req, res) => {
   try {
-    console.log('In the right place');
-
     const { email } = req.params;
 
     const patients = await Patient.find({ email }).sort({ createdAt: -1 });
